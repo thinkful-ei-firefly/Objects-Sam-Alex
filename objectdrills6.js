f8b8af0e40a8483773838c0a0a987066dc020bdd
Sam Walker <jack.nasal@gmail.com>
2019-07-10 15:09:04 -0400

    finished objectdrill6

--- objectdrills6.js
+++ objectdrills6.js
@@ -1,18 +1,33 @@
+'use strict';
+
+const translator = {
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5
+};
+
 const decode = message => {
-    const arr = message.split(" ");
+  const arr = message.split(' ');
   
-    const answer = arr.map(word => {
-      if (word[0] === "a") {
-        return word[2];
-      } else if (word[0] === "b") {
-        return word[3];
-      } else if (word[0] === "c") {
-        return word[4];
-      } else if (word[0] === "d") {
-        return word[5];
-      } else {
-        return " ";
-      }
-    });
-    return answer.join(" ");
-  };
+  const answer = arr.map(word => {
+    // if (word[0] === 'a') {
+    //   return word[2];
+    // } else if (word[0] === 'b') {
+    //   return word[3];
+    // } else if (word[0] === 'c') {
+    //   return word[4];
+    // } else if (word[0] === 'd') {
+    //   return word[5];
+    // } else {
+    //   return ' ';
+    // }
+    if (word[0] !== 'a' && word[0] !== 'b' && word[0] !== 'c' && word[0] !== 'd') {
+      return ' ';
+    }
+    return word[translator[word[0]]-1];
+  });
+  return answer.join('');
+};
+
+console.log(decode('craft block argon meter bells brown croon droop'));
