af014c1e3be327f87dfc4791ee479620ef533d79
ahyunlee <ahyunlee@umich.edu>
2019-07-10 08:32:22 -0700

    last two drills

---
+++ object-delete.js
@@ -0,0 +1,13 @@
+const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  };
+  
+  function keyDeleter(obj) {
+    // your code here
+    delete obj.foo;
+    delete obj.bar;
+    return obj;
+  }
