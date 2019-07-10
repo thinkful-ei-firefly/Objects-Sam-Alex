761239ce6ff277fe4b563f1438018e7c5c3731d9
ahyunlee <ahyunlee@umich.edu>
2019-07-10 09:20:38 -0700

    validate object

---
+++ object - validate.js
@@ -0,0 +1,31 @@
+// running the function with `objectA` and `expectedKeys`
+// should return `true`
+const objectA = {
+    id: 2,
+    name: 'Jane Doe',
+    age: 34,
+    city: 'Chicago',
+  };
+  
+  // running the function with `objectB` and `expectedKeys`
+  // should return `false`
+  const objectB = {
+    id: 3,
+    age: 33,
+    city: 'Peoria',
+  };
+  
+  const expectedKeys = ['id', 'name', 'age', 'city'];
+  
+  function validateKeys(object, expectedKeys) {
+    // your code goes here
+    const object1 = Object.keys(object);
+    if (object1.length !== expectedKeys.length) {
+        return false;
+    } 
+    for (let i = 0; i < expectedKeys.length; i++) {
+        if (!object1.includes(expectedKeys[i]))
+        return false;
+    }
+    return true;
+  }
