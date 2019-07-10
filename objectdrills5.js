29ebda3da6413f8e719c340c40de2116df7ef43c
ahyunlee <ahyunlee@umich.edu>
2019-07-10 11:39:08 -0700

    drill 5

---
+++ objectdrills5.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const spiderman = {
+    name: 'Peter',
+    job: 'photographer',
+    boss: 'JJJ',
+  };
+  const superman = {
+    name: 'Clark',
+    job: 'journalist',
+    boss: 'LL',
+  };
+  const ironman = {
+    name: 'RDJ',
+    job: 'dead',
+  };
+  const hulk = {
+    name: 'Bruce',
+    job: 'scientist',
+    boss: 'BW'
+  };
+  
+  const everyone = [spiderman, superman, ironman, hulk];
+  
+  function writeJobs() {
+    for (let i=0; i < everyone.length; i++) {
+        if (everyone[i].boss) {
+      console.log(`${everyone[i].job} ${everyone[i].name} reports to ${everyone[i].boss}`);
+    } else {
+        console.log(`${everyone[i].job} ${everyone[i].name} doesn't report to nobody`);
+    }
+    }
+  }
+
+writeJobs();
