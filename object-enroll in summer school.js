4f19b37bd5424c11338f90270997e8f80120c26d
ahyunlee <ahyunlee@umich.edu>
2019-07-10 08:55:48 -0700

    enrolled in summer school

---
+++ object-enroll in summer school.js
@@ -0,0 +1,30 @@
+const studentData = [
+    {
+      name: 'Tim',
+      status: 'Current student',
+      course: 'Biology',
+    },
+    {
+      name: 'Sue',
+      status: 'Withdrawn',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Liz',
+      status: 'On leave',
+      course: 'Computer science',
+    },
+  ];
+  
+  function enrollInSummerSchool(students) {
+    // your code here
+    const summerStudents = [];
+    for (let i = 0; i < students.length; i++) {
+        summerStudents.push({
+            name: students[i].name,
+            status: 'In summer school',
+            course: students[i].course,
+        })
+    }
+    return summerStudents;
+  }
